b202dc8e3295fc16a159f3facd1900ed231376ca
Mark Biegel <markbiegel@gmail.com>
2017-06-26 10:54:05 +1000

    admin view

--- app/controllers/spree/store_controller_decorator.rb
+++ app/controllers/spree/store_controller_decorator.rb
@@ -2,7 +2,13 @@ Spree::StoreController.class_eval do
   before_filter :maintenance_mode
 
   def maintenance_mode
-    render file: "public/maintenance_mode.html", layout: false, status: 503 if Spree::Config[:maintenance_mode]
-    return
+    if Spree::Config[:maintenance_mode]
+      unless spree_current_user.admin?
+        render file: "public/maintenance_mode.html", layout: false, status: 503
+        return
+      else
+        render partial: "shared/maintenance_mode"
+      end
+    end
   end
 end
